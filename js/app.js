09f1e7f9c926b05cbe4e664d83b0133a6bf71ee9
YeinThu <yeinthuuu@gmail.com>
2020-11-06 19:19:53 -0800

    Second Commit

--- js/app.js
+++ js/app.js
@@ -41,21 +41,40 @@ const addPhraseToDisplay = (phr) => {
 const checkLetter = (button) => {
   const lis = phrase.querySelector('ul').children;
 
-  if (lis) {
-    for (let i = 0; i < lis.length; i++) {
-      const current = lis[i];
+  let match = null;
 
-      if (current.textContent === button.textContent) {
-        current.classList += ' show';
-        button.setAttribute('disabled', 'true');
-        button.className = 'chosen';
-      }
-      else {
-        
-      }
+  for (let i = 0; i < lis.length; i++) {
+    const current = lis[i];
+
+    if (current.textContent === button.textContent) {
+      current.classList += ' show wiggle';
+      match = true;
     }
   }
 
+  return match;
+}
+
+// Check If Game Has Been Won / Lost
+const checkWin = () => {
+  const letterLis = phrase.querySelectorAll('ul .letter');
+  const showLis = phrase.querySelectorAll('ul .show');
+  const overlay = document.querySelector('#overlay');
+
+  if (letterLis.length === showLis.length) {
+    overlay.className = 'win';
+    startGameBtn.textContent = 'Play Again';
+    overlay.querySelector('.message').textContent = 'You\'ve Won!';
+    overlay.style.display = 'flex';
+  } 
+  else if (missedGuesses > 4) {
+    overlay.className = 'lose';
+    startGameBtn.textContent = 'Play Again';
+    overlay.querySelector('.message').textContent = 'You\'ve Lost...';
+    overlay.style.display = 'flex';
+  }
+
+  startGameBtn.addEventListener('click', resetGame);
 }
 
 // Listen For Start Game Button Click
@@ -70,14 +89,52 @@ startGameBtn.addEventListener('click', () => {
 
 });
 
+// Reset Game
+const resetGame = () => {
+  // Set Missed Guess To 0
+  missedGuesses = 0;
+
+  // Fill Live Hearts
+  let liveHearts = document.querySelectorAll('.tries img');
+  for (let i = 0; i < liveHearts.length; i++) {
+    liveHearts[i].src = 'images/liveHeart.png';
+  }
+
+  // Refresh Keyboard
+  qwerty.innerHTML = `
+  <div class="keyrow">
+    <button>q</button><button>w</button><button>e</button><button>r</button><button>t</button><button>y</button><button>u</button><button>i</button><button>o</button><button>p</button>
+  </div>
+  <div class="keyrow">
+    <button>a</button><button>s</button><button>d</button><button>f</button><button>g</button><button>h</button><button>j</button><button>k</button><button>l</button>
+  </div>
+  <div class="keyrow">
+    <button>z</button><button>x</button><button>c</button><button>v</button><button>b</button><button>n</button><button>m</button>
+  </div>
+  `;
+}
+
 // Listen For OnScreen Keyboard Click
 qwerty.addEventListener('click', e => {
   if (e.target.tagName === 'BUTTON') {
     // Get Letter User Clicked On
     const userValue = e.target;
+
+    // Add Class & Disabled To Button
+    userValue.classList += ' chosen';
+    userValue.setAttribute('disabled', 'true');
+
     // Call Check Letter
-    checkLetter(userValue);
-    
+    if (!checkLetter(userValue)) {
+      userValue.style.backgroundColor = 'orange';
+      missedGuesses++;
+
+      // Subtract Heart
+      let liveHearts = document.querySelectorAll('.tries img');
+      liveHearts[`${missedGuesses - 1}`].src = 'images/lostHeart.png';
+    }
   }
+
+  checkWin();
 })
 
